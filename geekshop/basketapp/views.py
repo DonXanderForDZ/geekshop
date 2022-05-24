7eecf94066c5b4032f700c654a62bc03a1eac345
DonXanderForDZ <77668107+DonXanderForDZ@users.noreply.github.com>
2022-05-24 16:03:22 +0300

    Revert "Lesson5"

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,35 +1,19 @@
+from audioop import reverse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http.response import HttpResponseRedirect
 from django.urls import reverse
-from basketapp.models import Basket
-from mainapp.models import Product
+from .models import Basket
 
 # Create your views here.
 
 def view (request):
-    return render(request, 'basketapp/view.html', context={
-        'basket': Basket.objects.filter(user=request.user)
-    })
+    render(request, 'basketapp/view.html')
 
-def add (request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-    basket = Basket.objects.filter(user=request.user, product=product)
-    if basket:
-        basket_item = basket[0]
-        basket_item.quantity += 1
-        basket_item.save()
-    else:
-        basket_item = Basket(user=request.user, product=product)
-        basket_item.save()
+def add (request):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', reverse('index')))
 
-    
 def remove (request, basket_id):
     basket = get_object_or_404(Basket, pk=basket_id)
-    basket.quantity -= 1
-    if not basket.quantity:
-        basket.delete()
-    else:
-        basket.save()
+    basket.delete
     return HttpResponseRedirect(reverse('basket:view'))
